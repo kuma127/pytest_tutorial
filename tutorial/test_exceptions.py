40b70690f54cf5c641f3cccf0c8b133a22ca527c
kumappp <kumagai27207@gmail.com>
2022-07-16 01:05:56 +0900

    ch2練習問題

---
+++ tutorial/test_exceptions.py
@@ -0,0 +1,12 @@
+import py
+import pytest
+import tasks
+from tasks.api import UninitializedDatabase
+
+def test_count_raises():
+    with pytest.raises(UninitializedDatabase):
+        tasks.count()
+
+def test_unique_id_raises():
+    with pytest.raises(UninitializedDatabase):
+        tasks.unique_id()
